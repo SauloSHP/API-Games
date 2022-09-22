f07f56c620404149426f85c679faa5eee432e1b1
Saulo <saulo972shp@hotmail.com>
2022-09-21 23:35:04 -0300

    GetMapping All e ById

--- src/main/java/br/gamesapp/service/GameService.java
+++ src/main/java/br/gamesapp/service/GameService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class GameService {
@@ -13,10 +15,22 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @GetMapping("/games")
+    public List<Game> listar() {
+        return gameRepository.findAll();
+    }
+
+    @GetMapping("/games/{id}")
+    public Game listarJogoPorId(@PathVariable Long id){
+        return gameRepository.findById(id).get();
+    }
+
     @PostMapping("/games")
     @ResponseStatus(HttpStatus.CREATED)
     public Game save(@RequestBody Game game) {
         return gameRepository.save(game);
     }
+
+
 
 }
